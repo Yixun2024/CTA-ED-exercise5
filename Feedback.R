20a65ef27adfa449573bce04956bbb3163021db7
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-11 15:16:26 +0100

    Feedback on formative

---
+++ Feedback.R
@@ -0,0 +1,28 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+# the code is fine but you didn't explain your choice of books
+
+# question 2
+# -----------------------------------------------------------
+# again the code is fine but there is no interpretation of the analyses or results
+
+# question 3
+# -----------------------------------------------------------
+# you didn't actually run the PreText analyses, and therefore also didn't interpret the outputs.
+
+# overall code quality: 
+# -----------------------------------------------------------
+# Overall you re-used the exercise code pretty well
+# But you need to knit your document in order for me to assess it
+# you also need to comment your code more (using in-code comments preceded by hashtags)
+# make sure code chunks that you want to run do not have 'eval=FALSE' in the code chunk specification: that line tells R not to run this code chunk. This is what happened with the PreText analysis.
+
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# You didn't include any interpretation; this is a non-negotiable for the final assignment. 
+# If you only run the code but do not explain what the analyses means for the text and how to interpret 
+# and learn from the results and the modelling, then your assignment will be failed... 
+# Make sure you focus a lot more on interpretation and analyses in your final assignment!
